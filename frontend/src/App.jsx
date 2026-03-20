348cea1712d067ae61032471ad89d62feb6e48ac
Swayam Gaur <goura0775@gmail.com>
2026-03-20 16:27:41 +0530

    kljh

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,8 @@
-import { useState } from 'react'
-import Login from './components/Login'
-import Dashboard from './components/Dashboard'
-import { getToken, getUser, clearSession } from './services/auth.service'
-import './App.css'
+import React from 'react'
 
 function App() {
-  const [user, setUser] = useState(() => getToken() ? getUser() : null)
-
-  const handleLogin = (userData) => {
-    setUser(userData)
-  }
-
-  const handleLogout = () => {
-    clearSession()
-    setUser(null)
-  }
-
   return (
-    <>
-      {!user ? (
-        <Login onLogin={handleLogin} />
-      ) : (
-        
-        <Dashboard user={user} onLogout={handleLogout} />
-      )}
-    </>
+    <div>App</div>
   )
 }
 
