12377f5e844f427996ef6a469d2e11391e323c2a
Swayam Gaur <goura0775@gmail.com>
2026-08-26 14:54:23 +0530

    h

--- frontend/src/Redux/api.jsx
+++ frontend/src/Redux/api.jsx
@@ -18,7 +18,8 @@ const getToken = () => localStorage.getItem('gg_token');
 // Base query with authentication
 const baseQuery = fetchBaseQuery({
 
-    baseUrl: 'http://localhost:5010/',
+    // baseUrl: 'http://localhost:5010/',
+    baseUrl: 'https://glow-green.onrender.com/',
     // baseUrl: 'https://glow.green.api.amaxjobs.com/',
     prepareHeaders: (headers) => {
         const token = getToken();
